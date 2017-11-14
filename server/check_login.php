ba1d39b56a6c622c3409777d276109d48aae81aa
Carlos Tolentino <carlos.tolentinoe@gmail.com>
2017-11-14 12:03:48 -0400

    Creacion de la conexion con la base de dato

--- server/check_login.php
+++ server/check_login.php
@@ -1,6 +1,12 @@
 <?php
 
+//Importar la clase conexion
+require_once('conexion.php');
 
+  conexion = new ConectorBD('localhost', 'root', 'Tolenk2311');
+  conexion->initConexion('agenda_nextu');
+
+  
 
 
  ?>
--- server/create_user.php
+++ server/create_user.php
@@ -1,7 +1,8 @@
 <?php
+  //Importar la clase conexion
+  require_once('conexion.php');
 
-
-
-
+    conexion = new ConectorBD('localhost', 'root', 'Tolenk2311');
+    conexion->initConexion('agenda_nextu');
 
  ?>
